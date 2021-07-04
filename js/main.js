0f43d1bc02a8d5571e2fd4e2ae91641e3cabb177
David-LBautista <196w0007@itszongolica.edu.mx>
2021-07-04 14:37:03 -0500

    edicion de cita, inicio de filtrado de pacientes segun la busqueda

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
 
+// MODO EDICION
+let editando;
+
 
 
 
@@ -29,6 +32,15 @@ class Citas {
     agregarCita(cita){
         this.citas = [...this.citas, cita];
         console.log(this.citas);
+    }
+
+    eliminarCita(id){
+        this.citas = this.citas.filter( cita => cita.id !== id);
+        console.log(this.citas);
+    }
+
+    edicionCita(citaActualizada){
+        this.citas = this.citas.map( cita => cita.id === citaActualizada.id ? citaActualizada : cita);
     }
 
 }
@@ -66,20 +78,72 @@ class UI {
             const { paciente , servicio, telefono, fecha, hora, observaciones, id} = cita;
 
             const divCita = document.createElement('div');
-            divCita.classList.add('card');
+            divCita.classList.add('card', 'p-3');
             divCita.dataset.id = id;
-            divCita.innerHTML = `
-            <div class="card-body col-sm-12 col-md-12">
-                <h5 class="card-title fs-2">${paciente}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${servicio}</h6>
-                <p><span class"font-weight-bolder">Telefono:</span> ${telefono}</p> 
-                <p><span class"font-weight-bolder">Fecha:</span> ${fecha}</p> 
-                <p><span class"font-weight-bolder">Hora:</span> ${hora}</p> 
-                <p><span class"font-weight-bolder">Observaciones:</span> ${observaciones}</p> 
-            
-                
-                </div>
+
+            const pacienteParrafo = document.createElement('h2');
+            pacienteParrafo.classList.add('card-title', 'font-weight-bolder', 'fs-3');
+            pacienteParrafo.textContent = paciente;
+
+            const servicioParrafo = document.createElement('p', 'fs-6');
+            servicioParrafo.style.color = "red";
+            servicioParrafo.innerHTML = `
+                <span class="fw-bolder">${servicio}</span> 
             `;
+
+            const telefonoParrafo = document.createElement('p', 'fs-6');
+            telefonoParrafo.innerHTML = `
+                <span class="fw-bolder">Telefono:</span> ${telefono}
+            `;
+
+            const fechaParrafo = document.createElement('p', 'fs-6');
+            fechaParrafo.innerHTML = `
+                <span class="fw-bolder">Fecha:</span> ${fecha}
+            `;
+
+            const horaParrafo = document.createElement('p', 'fs-6');
+            horaParrafo.innerHTML = `
+                <span class="fw-bolder">Hora:</span> ${hora}
+            `;
+            const observacionesParrafo = document.createElement('p', 'fs-6');
+            observacionesParrafo.innerHTML = `
+                <span class="fw-bolder">Observaciones:</span> ${observaciones}
+            `;
+            
+
+
+
+            const btnEditar = document.createElement('button');
+            const btnEliminar = document.createElement('button');
+            const botonesDiv = document.createElement('div');
+
+            btnEditar.classList.add('btn', 'btn-outline-success');
+            btnEditar.style.marginRight = '10px';
+
+            btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+            </svg>`;
+
+            btnEditar.onclick = () => editarCita(cita);
+
+            btnEliminar.classList.add('btn', 'btn-outline-danger');
+            btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>`;
+            btnEliminar.onclick = () => eliminarCita(id);
+
+            botonesDiv.appendChild(btnEditar);
+            botonesDiv.appendChild(btnEliminar);
+
+            divCita.appendChild(pacienteParrafo);
+            divCita.appendChild(servicioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(observacionesParrafo);
+            
+            divCita.appendChild(botonesDiv);
 
 
             contenedorCitas.appendChild(divCita);
@@ -157,14 +221,30 @@ function nuevaCita(e){
         return;
     }
 
-    //Generamos un id unico
-    citaObj.id = Date.now();
+    if (editando) {
+        ui.imprimirAlerta('Se editó correctamente');
 
-    // Creamos la nueva cita
-    adminCitas.agregarCita({...citaObj});
+        //pasar el objeto de la cita a edicion
+        adminCitas.edicionCita({...citaObj});
 
-    // Mensaje de agregado correctamente
-    ui.imprimirAlerta('Se agrego la cita correctamente');
+        //regresar el texto del boton a su estado original
+        formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
+
+        //quitando modo edicion
+        editando = false;
+
+    }else{
+
+        //Generar un id unico
+        citaObj.id = Date.now();
+
+        //Creamos la nueva cita
+        adminCitas.agregarCita({...citaObj});
+
+        //Mensaje de agregado correctamente
+        ui.imprimirAlerta('Se agrego correctamente');
+    }
+
 
     // Reiniciamos el formulario y el objeto
     reiniciarObj();
@@ -193,6 +273,35 @@ function eliminarCita(id){
 
     // Refresque las citas del DOM
     ui.mostrarCita(adminCitas);
+
+}
+
+function editarCita(cita){
+    const { paciente, servicio, telefono, fecha, hora, observaciones, id } = cita;
+
+    //llenamos los input
+    pacienteInput.value = paciente;
+    servicioInput.value = servicio;
+    telefonoInput.value = telefono;
+    fechaInput.value = fecha;
+    horaInput.value = hora;
+    observacionesInput.value = observaciones;
+
+    //Llenamos el objeto
+    citaObj.paciente = paciente;
+    citaObj.servicio = servicio;
+    citaObj.telefono = telefono;
+    citaObj.fecha = fecha;
+    citaObj.hora = hora;
+    citaObj.observaciones = observaciones;
+    citaObj.id = id;
+
+
+    
+    //Cambiamos el texto del boton de guardar
+    formulario.querySelector('button[type="submit"]').textContent = 'Guardar Cambios';
+
+    editando = true;
 
 }
 
